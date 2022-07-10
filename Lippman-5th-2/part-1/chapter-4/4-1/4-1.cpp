b82f367fd43a19a59b3578ba97e1775357a38b13
Cicada <cicada@44>
2022-07-10 00:54:28 -0700

    End of 4-1

---
+++ Lippman-5th-2/part-1/chapter-4/4-1/4-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int result = 5 + 10 * 20 / 2; // 105
+
+    cout << "res - " << result << endl;
+
+    return 0;
+}
---
+++ Lippman-5th-2/part-1/chapter-4/4-1/4-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v(10);
+
+    // (2)* vec(1).begin()
+    // (2)* vec(1).begin() (3)+ 1
+
+    return 0;
+}
